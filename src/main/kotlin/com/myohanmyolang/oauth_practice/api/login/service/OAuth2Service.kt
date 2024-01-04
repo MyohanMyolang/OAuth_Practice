d6b24407aae721c19b91d879fbc9042dd327ee25
Myolang <myohanmyolang@gmail.com>
2024-01-05 04:20:31 +0900

    ADD OAuthClientContainer getClient TraillingLambda

--- src/main/kotlin/com/myohanmyolang/oauth_practice/api/login/service/OAuth2Service.kt
+++ src/main/kotlin/com/myohanmyolang/oauth_practice/api/login/service/OAuth2Service.kt
@@ -12,17 +12,10 @@ class OAuth2Service(
 	private val socialMemberService: SocialMemberService,
 	private val jwtHelper: JwtHelper
 ) {
-
-	/**
-	 * NOTE:
-	 *  1. provider로 Factory에서 해당하는 Client 얻기
-	 *  2. 인가 코드로 AccessToken 얻기
-	 *  3. AccessToken으로 유저 정보 조회
-	 *  4. 유저 정보가 조회, 없다면 회원 가입 처리
-	 *  5. 정보를 토대로 JwtToken 발행
-	 */
 	fun login(authorizationCode: String, provider: OAuth2Provider) =
-		oAuthClientContainer.getClient(provider).getUserInfoByAuthorizationCode(authorizationCode)
+		oAuthClientContainer.getClient(provider){
+			getUserInfoByAuthorizationCode(authorizationCode)
+		}
 			.let { socialMemberService.registerIfAbsent(it, provider) }
 			.let { jwtHelper.generateAccessToken(it.nickname) }
 
--- src/main/kotlin/com/myohanmyolang/oauth_practice/client/OAuthClientContainer.kt
+++ src/main/kotlin/com/myohanmyolang/oauth_practice/client/OAuthClientContainer.kt
@@ -2,8 +2,11 @@ package com.myohanmyolang.oauth_practice.client
 
 import com.myohanmyolang.oauth_practice.domain.entity.OAuth2Provider
 
-class OAuthClientContainer (
+class OAuthClientContainer(
 	private val clientList: Map<OAuth2Provider, OAuthClient>
-){
-	fun getClient(provider: OAuth2Provider) = clientList[provider] ?: TODO("FrontEnd 요청 확인 또는 Configuration 확인")
+) {
+	fun <T> getClient(provider: OAuth2Provider, func: OAuthClient.() -> T): T {
+		clientList[provider]?.let { return func.invoke(it) }
+			?: TODO("FrontEnd 요청 확인 또는 Configuration 확인")
+	}
 }
